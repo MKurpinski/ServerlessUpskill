58670c44b5957ff3092a7243518a2a56cb2ac8d9
Michał Kurpiński <michal.kurpinski96@gmail.com>
2020-01-13 17:37:39 +0100

    Delete old, not needed projects

--- Upskill/Application.Commands/Commands/RecompensateApplicationProcessCommand.cs
+++ Upskill/Application.Commands/Commands/RecompensateApplicationProcessCommand.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Application.Commands.Commands
+﻿namespace Application.Commands.Commands
 {
     public class RecompensateApplicationProcessCommand
     {
